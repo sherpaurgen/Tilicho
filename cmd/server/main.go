975cfa606e8fc43a292008c4f97d8fed30d69e01
urgen sherpa <sherpaurgen@users.noreply.github.com>
2024-03-16 23:06:09 +0545

    main: Add postgresql connection pool , initialized in main.go Run function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-// run will instantiate and startup the go application
+	"github.com/sherpaurgen/Tilicho/internal/database" //importing from internal/database pkg
+)
+
+// run will instantiate and startup the project app
 func Run() error {
 	fmt.Println("Starting...")
+	db, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("cannot connect to database")
+		return err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.Ping(ctx)
+	if err != nil {
+		log.Printf("failed to ping database: %v", err)
+		return err
+	}
+	fmt.Println("database connection [ok]")
 	return nil
 }
 
--- internal/database/db.go
+++ internal/database/db.go
@@ -3,7 +3,45 @@ package database
 import (
 	"context"
 	"errors"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+// pgx pgxpool is used https://github.com/jackc/pgx here because the lib pq is in archive mode and recommends new projject to use pgx
+type Database struct {
+	Pool *pgxpool.Pool
+}
+
+func NewDatabase() (*Database, error) {
+	connectionString := fmt.Sprintf(
+		"postgres://user=%s password=%s host=%s port=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("SSL_MODE"),
+	)
+
+	pool, err := pgxpool.New(context.Background(), connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to database: %v", err)
+	}
+
+	return &Database{Pool: pool}, nil
+}
+
+func (db *Database) Ping(ctx context.Context) error {
+	//PingContext verifies a connection to the database is still alive, establishing a connection if necessary.
+	// basically for healthcheck
+	err := db.Pool.Ping(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to ping database: %w", err)
+	}
+	return nil
+}
 
 type Apartment struct {
 	apartment_id   string
