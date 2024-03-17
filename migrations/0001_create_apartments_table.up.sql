cdc41f8f5f6cc82553cb32f977ef99ccf7dc5fe4
urgen sherpa <sherpaurgen@users.noreply.github.com>
2024-03-17 22:01:14 +0545

    feature: add migratedb function to initialize table

---
+++ migrations/0001_create_apartments_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS apartments (
+    apartment_id UUID,
+    apartment_name varchar(255),
+    address varchar(255),
+    city varchar(255),
+    state varchar(255),
+    zip_code varchar(255)
+);
